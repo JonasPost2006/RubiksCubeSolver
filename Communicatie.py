da3c41de138bdd8a4f47b9187e175d9b15bf6e86
JonasPost2006 <43406465+JonasPost2006@users.noreply.github.com>
2023-12-06 16:45:59 +0100

    Begin

---
+++ Communicatie.py
@@ -0,0 +1,18 @@
+#! D:\Jonas\Coderclass\CubeSolver\RubiksCubeSolver\venv\Scripts\python.exe
+
+import serial
+import time
+
+port = 'COM3' #verander naar goede uitgang
+
+ser = serial.Serial(port, 9600, timeout=1)
+
+def rotate_stepper():
+    ser.write(b'user_input') #Checken of dit 'user_input' stuurt of de echte input
+    time.sleep(2) #?????
+
+user_input = input("Geef input")
+if user_input == True:
+    rotate_stepper()
+
+ser.close()
--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-#! D:\Jonas\Coderclass\CubeSolver\RubiksCubeSolver\venv\Scripts\python.exe
+from typing import NewType, Tuple, List, TypeVar, Union
+from itertools import permutations
 
-import serial
-import time
+from .pieces import Corner, Edge, CORNER_TO_UPFRONT, EDGE_TO_UPFRONT 
 
-port = 'COM3' #verander naar goede uitgang
+Colour = NewType("Colour", Tuple[int, int, int])
 
-ser = serial.Serial(port, 9600, timeout=1)
+YELLOW = Colour((255, 255, 0))
+GREEN = Colour((0, 255, 0))
+RED = Colour((255, 0, 0))
+BLUE = Colour((0, 0, 255))
+ORANGE = Colour((255, 126, 0))
+WHITE = Colour((255, 255, 255))
 
-def rotate_stepper():
-    ser.write(b'user_input') #Checken of dit 'user_input' stuurt of de echte input
-    time.sleep(2) #?????
+COLOUR_MAPPING = [("U", YELLOW), ("F", GREEN), ("L", RED), ("B", BLUE), ("R", ORANGE), ("D", WHITE)]
 
-user_input = input("Geef input")
-if user_input == True:
-    rotate_stepper()
-
-ser.close()
+class Cube:
+    def __init__(self, size:int):
+        self.size = size
+        self.faces = {face: self._generate_face(colour, size) for face, colour in COLOUR_MAPPING}
+    
+    def get_sticker(self, sticker: str) -> Colour:
+        for perm in permutations(sticker):
+            if "".join(perm) in EDGE_TO_UPFRONT:
+                return self.get_edge("".join(perm))[sticker[0]]
+            elif "".join(perm) in CORNER_TO_UPFRONT:
+                return self.get_corner("".join(perm))[sticker[0]]
+        raise ValueError(f"Geen geldige sticker: {sticker}")
+    
