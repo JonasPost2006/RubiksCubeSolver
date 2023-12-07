e35e140dc138b5f3c4b1d16b7091ef149017cbd6
JonasPost <jonas.post@coderclass.nl>
2023-12-07 14:04:07 +0100

    Communicatie vastgelegd

--- Communicatie.py
+++ Communicatie.py
@@ -3,16 +3,19 @@
 import serial
 import time
 
-port = 'COM3' #verander naar goede uitgang
+port = 'COM10' #verander naar goede uitgang
 
 ser = serial.Serial(port, 9600, timeout=1)
 
-def rotate_stepper():
-    ser.write(b'user_input') #Checken of dit 'user_input' stuurt of de echte input
-    time.sleep(2) #?????
 
-user_input = input("Geef input")
-if user_input == True:
-    rotate_stepper()
 
-ser.close()
+def write_read(data):
+    ser.write(bytes(data, 'utf-8')) #Checken of dit 'user_input' stuurt of de echte input
+    time.sleep(0.01) #?????
+    incommingData = ser.readline()
+    return incommingData
+
+while True:
+    value = input("Voer de oplossing in: ")
+    oplossing = write_read(value)
+    print(oplossing)
